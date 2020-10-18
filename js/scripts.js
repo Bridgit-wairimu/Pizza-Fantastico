d94c312f5cc7a211fc31b42fd3841a08dd77b436
Bridgit-wairimu <kirikabetty@gmail.com>
2020-10-18 10:23:52 -0700

    changed the var keyword to let

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,23 @@ $(document).ready(function(){
   
 
     function flavor() {
-      var selectFlavor = document.getElementById("flavor").value;
+      let selectFlavor = document.getElementById("flavor").value;
       return parseInt(selectFlavor);
     }
     function size() {
-      var selectSize = document.getElementById("size").value;
+      let selectSize = document.getElementById("size").value;
       return parseInt(selectSize);
     }
     function crust() {
-      var selectCrust = document.getElementById("crust").value;
+      let selectCrust = document.getElementById("crust").value;
       return parseInt(selectCrust);
     }
     function topping() {
-      var selectTopping = document.getElementById("topping").value;
+      let selectTopping = document.getElementById("topping").value;
       return parseInt(selectTopping);
     }
     function number() {
-      var numberOfPizzas = document.getElementById("number").value;
+      let numberOfPizzas = document.getElementById("number").value;
       return parseInt(numberOfPizzas);
     }
 
@@ -29,14 +29,14 @@ $(document).ready(function(){
       this.newTopping = topping;
       this.newNumber = number;
     }
-    var userInput = new order(flavor(), size(), crust(), topping(), number());
+    let userInput = new order(flavor(), size(), crust(), topping(), number());
 
-    var totalCost =
+    let totalCost =
       (userInput.newFlavor +
         userInput.newSize +
         userInput.newCrust +
         userInput.newTopping) *
-      userInput.newNumber;
+        userInput.newNumber;
       alert("Your charges for Pizza is " + + totalCost);
       alert("Your pizza will be delivered to you in an instant and thank you for choosing us");  
 
