004cc4d9ca5601e28e77a8f0977e931e8ad5229a
Bridgit-wairimu <kirikabetty@gmail.com>
2020-10-18 21:01:13 -0700

    modified

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
     let userInput = new order(flavor(),size(),crust(),topping(),number()); 
 
-    let totalCost = (userInput.newFlavor + userInput.newSize + userInput.newCrust + userInput.newTopping) *userInput.newNumber;
+    let totalCost = (userInput.newFlavor + userInput.newSize + userInput.newCrust + userInput.newTopping);
       alert("Your charges for pizza is " + + totalCost)
       alert("Your pizza will be delivered to you in an instant and thank you for choosing us");
 
