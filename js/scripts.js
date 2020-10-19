964a91732f70651a33cd3c98d300ee85c5b59ca3
Bridgit-wairimu <kirikabetty@gmail.com>
2020-10-18 22:18:20 -0700

    added a delivery cost statement in the alert box

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,45 @@
 $(document).ready(function(){
   
 
-    function flavor() {
-      let selectFlavor = document.getElementById("flavor").value;
-      return parseInt(selectFlavor);
-    }
-    function size() {
-      let selectSize = document.getElementById("size").value;
-      return parseInt(selectSize);
-    }
-    function crust() {
-      let selectCrust = document.getElementById("crust").value;
-      return parseInt(selectCrust);
-    }
-    function topping() {
-      let selectTopping = document.getElementById("topping").value;
-      return parseInt(selectTopping);
-    }
-    function number() {
-      let inputNumber = document.getElementById("number").value;
-      return parseInt(inputNumber);
-    }
+  function flavor() {
+    let selectFlavor = document.getElementById("flavor").value;
+    return parseInt(selectFlavor);
+  }
+  function size() {
+    let selectSize = document.getElementById("size").value;
+    return parseInt(selectSize);
+  }
+  function crust() {
+    let selectCrust = document.getElementById("crust").value;
+    return parseInt(selectCrust);
+  }
+  function topping() {
+    let selectTopping = document.getElementById("topping").value;
+    return parseInt(selectTopping);
+  }
+  function number() {
+    let inputNumber = document.getElementById("number").value;
+    return parseInt(inputNumber);
+  }
 
-    function order(flavor, size, crust, topping, number) {
-      this.newFlavor = flavor;
-      this.newSize = size;
-      this.newCrust = crust;
-      this.newTopping = topping;
-      this.newNumber = number;
-    }
+  function order(flavor, size, crust, topping, number) {
+    this.newFlavor = flavor;
+    this.newSize = size;
+    this.newCrust = crust;
+    this.newTopping = topping;
+    this.newNumber = number;
+  }
 
-    let userInput = new order(flavor(),size(),crust(),topping(),number()); 
+  let userInput = new order(flavor(),size(),crust(),topping(),number()); 
+    
+  let totalCost = (userInput.newFlavor + userInput.newSize + userInput.newCrust + userInput.newTopping);
+    alert("Your charges for pizza is " + + totalCost)
+    alert("Your pizza will be delivered to you in an instant and your delivery cost is sh.200 .Thank you for choosing us");
 
-    let totalCost = (userInput.newFlavor + userInput.newSize + userInput.newCrust + userInput.newTopping);
-      alert("Your charges for pizza is " + + totalCost)
-      
-      alert("Your pizza will be delivered to you in an instant and thank you for choosing us");
 
-  $("#formgroup").submit(function(execute) {
-    execute.preventDefault();
+    $("#formgroup")(function(execute) {
+  execute.preventDefault();
 
 });
 });
+
